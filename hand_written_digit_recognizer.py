30c8a6288a7498023a4aed3fd24995610969afb7
krishNav-eng <71910691+krishNav-eng@users.noreply.github.com>
2026-06-15 16:35:48 +0000

    day1

---
+++ hand_written_digit_recognizer.py
@@ -0,0 +1,48 @@
+from  network import *
+from mnist_loader import *
+import struct
+from array import array
+from os.path  import join
+import random
+import matplotlib.pyplot as plt
+
+input_path = './handwritten_digit_data/handwritten_digit_images'
+training_images_filepath = join(input_path, 'train-images-idx3-ubyte/train-images-idx3-ubyte')
+training_labels_filepath = join(input_path, 'train-labels-idx1-ubyte/train-labels-idx1-ubyte')
+test_images_filepath = join(input_path, 't10k-images-idx3-ubyte/t10k-images-idx3-ubyte')
+test_labels_filepath = join(input_path, 't10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte')
+
+
+mnist_dataloader = MnistDataLoader(training_images_filepath, training_labels_filepath, test_images_filepath, test_labels_filepath)
+(x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
+
+
+def show_images(images, title_texts):
+    cols = 5
+    rows = int(len(images)/cols) + 1
+    plt.figure(figsize=(30,20))
+    index = 1    
+    for x in zip(images, title_texts):        
+        image = x[0]        
+        title_text = x[1]
+        plt.subplot(rows, cols, index)        
+        plt.imshow(image, cmap=plt.cm.gray)
+        if (title_text != ''):
+            plt.title(title_text, fontsize = 15);        
+        index += 1
+
+
+images_to_show = []
+titles_to_show = []
+for i in range(0, 10):
+    r = random.randint(1, 60000)
+    images_to_show.append(x_train[r])
+    titles_to_show.append('training image [' + str(r) + '] = ' + str(y_train[r]))    
+
+for i in range(0, 5):
+    r = random.randint(1, 10000)
+    images_to_show.append(x_test[r])        
+    titles_to_show.append('test image [' + str(r) + '] = ' + str(y_test[r]))    
+
+show_images(images_to_show, titles_to_show)
+plt.show()
